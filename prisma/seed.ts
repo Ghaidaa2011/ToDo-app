c62325fe786ac7739818e062f9bc4c27bb20e104
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2024-10-01 19:40:53 +0300

    done

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,15 @@
 import { PrismaClient } from '@prisma/client'
-import {faker} from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.todo.createMany({
-    data: Array.from({length: 25}, ()=>({
-    title: faker.lorem.words({min:2, max:5}),
-    body:  faker.lorem.words({min:1, max:10}),
-    
-  }))
-  }) 
-  // await prisma.user.createMany({
-  //   data: Array.from({length: 20}, ()=>({
-  //   email: faker.internet.email(),
-  //   name: faker.internet.userName(),
-  //   address: {
-  //     street:  faker.location.street(),
-  //     city: faker.location.city(),
-  //     state: faker.location.state(),
-  //     zip: faker.location.zipCode(),
-  //   }
-  // }))
+  // await prisma.todo.createMany({
+  //   data: Array.from({ length: 25 }, () => ({
+  //     title: faker.lorem.words({ min: 2, max: 5 }),
+  //     body: faker.lorem.words({ min: 1, max: 10 }),
+  //     user_id: "user_2mkyDGivIcbfSA7e9iTwMFi6b9V",
+  //   }))
   // })
 }
 
